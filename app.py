ba6331fce4af88dfff12a19eec9cc098d3ff76e9
Tirupati Reddy Bonam <TirupatiReddy.Bonam@Citrix.com>
2022-08-25 20:27:31 +0530

    files added

---
+++ app.py
@@ -0,0 +1,35 @@
+# app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+countries = [
+    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
+    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
+    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
+]
+
+
+def _find_next_id():
+    return max(country["id"] for country in countries) + 1
+
+
+@app.get("/countries")
+def get_countries():
+    resp = jsonify(countries)
+    resp.headers['Access-Control-Allow-Origin'] = 'Tirupati'
+    return resp
+
+
+@app.post("/countries")
+def add_country():
+    if request.is_json:
+        country = request.get_json()
+        country["id"] = _find_next_id()
+        countries.append(country)
+        return country, 201
+    return {"error": "Request must be JSON"}, 415
+
+
+if __name__ == '__main__':
+    app.run(port=7777)
